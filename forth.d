ba0cab5caf5b22212d78947151dfe29702b27bfb
jarlvs <premshinde13@gmail.com>
2016-05-30 15:39:36 +0530

    forth.d lost some of its data

--- forth.d
+++ forth.d
@@ -4,22 +4,3 @@ nlt44;n lk4nnlkng 5lknnn
 fkkjbrj5l l5hhio5 l5kl5l
 fkggi44 i5toi5t5t5
 lkttlno itooi5o6oi
-#include<iostream>
-using namespace std;
-int sum(int num){
-	if(num>1)
-    return num+sum(num-1);
-    else 
-    	return num;
-}
-
-int main()
-{ 
-	int a,b;
-	cin>>a;
-	b=sum(a);
-	cout<<b<<endl;
-
-	return 0;
-	
-}
